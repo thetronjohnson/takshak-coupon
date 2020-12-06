6a2d00579c51f8c2459708003b488c8c1c1da037
Kiran Johns <kiranjohns369@gmail.com>
2020-12-06 17:10:41 +0530

    Update settings.py

--- takshakcoupons/settings.py
+++ takshakcoupons/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6&2mwejj@=l%g$x&rmnz227rge-nzojwj86mmx&-km1a&!68$q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
