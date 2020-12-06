2232941380fc001f7885fc98e709a53e3a774ce8
Kiran Johns <kiranjohns369@gmail.com>
2020-12-06 15:00:04 +0530

    auto coupon

--- coupons/models.py
+++ coupons/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Data(models.Model):
 	created_by = models.CharField(max_length=90)
 	generated_for = models.CharField(max_length=100)
-	coupon_no = models.AutoField()
+	coupon_no = models.BigAutoField()
 	amount = models.CharField(max_length=100)
 
 	def __repr__(self):
